72c6585e419e2fe59817ae9948dd124031010dfc
Your Name <you@example.com>
2023-11-26 13:58:25 +0530

    Java Lab Program v.1

---
+++ Program4.java
@@ -0,0 +1,13 @@
+package Program4;
+
+// Program 4: To find Square Root of a Number
+import java.lang.Math; // Required for Math
+
+public class Program4{
+	public static void main(String[] args){
+		double x=25;
+		double y;
+		y=Math.sqrt(x);
+		System.out.println("Y=" + y);
+	}
+}
---
+++ Program6.java
@@ -0,0 +1,17 @@
+package Program6;
+
+// Program 6: More on Math Class
+import java.lang.Math;
+
+public class Program6{
+	public static void main(String[] args){
+		// Max of two numbers
+		System.out.println("Max: " + Math.max(10,20));
+
+		// Min of two numbers
+		System.out.println("Min: " + Math.min(10,20));
+
+		// Absolute of a number
+		System.out.println("Abs: " + Math.abs(-4.7));
+	}
+}
