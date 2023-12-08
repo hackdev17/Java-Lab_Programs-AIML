d42ac59d23daca572a981d3d114b50cebefea083
Your Name <you@example.com>
2023-12-08 20:19:27 +0530

    Sepearated Lab Programs and Demo Programs

--- Lab/StackOp.java
+++ Lab/StackOp.java
@@ -4,57 +4,66 @@
  * Develop a java class method to illustrate stack operation
  */
 
- package stackop;
+package stackop;
 
- public class StackOp{
+public class StackOp{
 	private int[] stackArray;
-	private int[] top;
+	private int top;
 	private static final int MAX_SIZE=10;
 
-	public declare(){
+	public StackOp(){
 		stackArray=new int[MAX_SIZE];
 		top=-1;
 	}
 
 	public void push(int data){
 		if(isFull())
-			System.out.println("Stack is full, cannot Push !"+data);
+			System.out.println(data+" : Stack is full, cannot Push");
 		else{
 			stackArray[++top]=data;
-			System.out.println(data+"Pushed on the Stack !");
+			System.out.println(data+" : Pushed on to the Stack");
 		}
 	}
 
 	public int pop(){
 		if(isEmpty()){
-			System.out.println("Stack is empty, cannot Push !");
+			System.out.println("Stack is empty, cannot Pop !");
 			return -1;
 		}else{
-			int poppedItem=stackArray[top-1];
-			System.out.println(poppedItem+"Popped from the Stack");
+			int poppedItem=stackArray[top--];
+			System.out.println(poppedItem+" : Popped from the Stack");
 			return poppedItem;
 		}
 	}
 
 	public boolean isEmpty(){
-		return top=-1;
+		return top==-1;
 	}
 
 	public boolean isFull(){
-		return top=MAX_SIZE-1;
+		return top==MAX_SIZE-1;
 	}
 
 	public static void main(String[] args){
-		for(int i=1;i<11;i++)
-			stack.pop(5*i);
+		StackOp stack=new StackOp();
+		for(int i=0;i<=10;i++)
+			stack.push(5*i);
 
-		System.out.println("Is the Stack empty ?"+stack.Empty());
-		System.out.println("Is the Stack full ?"+stack.Full());
+		System.out.println("Is the Stack empty ? "+stack.isEmpty());
+		System.out.println("Is the Stack full ? "+stack.isFull());
 
-		System.out.println("Popping elements from the Stack :");
+		System.out.println("\nPopping elements from the Stack :");
 		while(!stack.isEmpty())
 			stack.pop();
-		System.out.println("Is the Stack empty ?"+stack.Empty());
-		System.out.println("Is the Stack full ?"+stack.Full());
+
+		System.out.println("Is the Stack empty ? "+stack.isEmpty());
+		System.out.println("Is the Stack full ? "+stack.isFull());
+
+		System.out.println("\nPopping elements from the Stack :");
+		while(!stack.isEmpty())
+			stack.pop();
+
+		System.out.println("Is the Stack empty ? "+stack.isEmpty());
+		System.out.println("Is the Stack full ? "+stack.isFull());
 	}
 }
