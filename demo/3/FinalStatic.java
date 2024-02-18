2f61560096ee2a4f1f65dc41f69a52074bbe1b0d
hackdev17 <you@example.com>
2024-02-18 15:37:18 +0530

    update

--- demo/3/FinalStatic.java
+++ demo/3/FinalStatic.java
@@ -1,12 +1,15 @@
 final class Constant{
-	double PI=3.1416;
+	final double PI=3.1416;
 }
 
 public class FinalStatic{
 	public static void main(String[] args){
 		Constant obj=new Constant();
 		System.out.println("The value of final class : "+obj.PI);
-		obj.PI=3.14159;
-		System.out.println("The value after changing : "+obj.PI);
+		try{
+			obj.PI=3.14159;
+		}catch(Exception e){
+			System.out.println("The value after changing : "+obj.PI);
+		}
 	}
 }
